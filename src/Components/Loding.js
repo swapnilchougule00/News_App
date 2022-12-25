02992af70587b0d38973ae12c7f889efa5338dfe
swapnil <swapnilchougule00@gmail.com>
2022-12-25 16:41:26 +0530

    minor bug fixed

--- src/Components/Loding.js
+++ src/Components/Loding.js
@@ -4,9 +4,9 @@ import './Loding.css'
 function loding() {
   return (
     <div className='loding'>
-        <div class="spinner">
-<div class="loader l1"></div>
-<div class="loader l2"></div>
+        <div className="spinner">
+<div className="loader l1"></div>
+<div className="loader l2"></div>
 </div>
     </div>
   )
